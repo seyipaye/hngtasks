a03d7db71f6f4695917262f06a34313939e4b19f
Seyi <seyipaye@gmail.com>
2022-11-05 06:01:05 +0100

    fix: settings

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'django-insecure-swrkezdjj&h_lhg)(5k3f*%=3tq%frewl*0@u3+kj9%ov&fv5m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
